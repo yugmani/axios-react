1e71a7f63bf1fa4dad2184baf364df137e5a6d89
Himalaya <yugmani@yahoo.com>
2021-03-03 00:30:48 -0800

    axios:Done

---
+++ client/src/components/DataFetching.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function DataFetching() {
+  const [post, setPost] = useState({});
+
+  const [id, setId] = useState(1);
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1);
+
+  const handleClick = () => {
+    setIdFromButtonClick(id);
+  };
+  useEffect(() => {
+    axios
+      .get(`http://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
+      .then((res) => {
+        console.log(res);
+        setPost(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [idFromButtonClick]);
+
+  return (
+    <div>
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <button type="button" onClick={handleClick}>
+        Fetch Post
+      </button>
+      <div>{post.title}</div>
+    </div>
+  );
+}
+
+export default DataFetching;
